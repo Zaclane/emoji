04caecbc926d5003acfde3777387464e983faa41
Zac Lane <zac@movista.com>
2019-05-18 14:14:48 -0500

    notes added

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ http.createServer((request, response) => {
     response.write(emoji.random().emoji);
     response.end();
 }).listen(8080);
+
+
+/*
+
+npm init -y 
+npm install --save *package name*
+.gitignore -node_modules
+git init
+git add .
+git commit -m ""
